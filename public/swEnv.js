ad23754d49e74e712ce93a2614a4e2a89dc362a1
shaikhusama109 <shaikhusama109@gmail.com>
2024-10-13 19:24:21 +0500

    code cleanup

---
+++ public/swEnv.js
@@ -0,0 +1,9 @@
+const swEnv = {
+    VITE_API_KEY: 'AIzaSyAjzugaRZRa8r_0rtZG08fGvu-f7o4_wqc',
+    VITE_PROJECT_ID: 'saylani2nd',
+    VITE_MESSAGING_SENDER_ID: '1009546185453',
+    VITE_APP_ID: '1:1009546185453:web:0e699e037cb089ed83dae4',
+    VITE_AUTH_DOMAIN: 'saylani2nd.firebaseapp.com',
+    VITE_STORAGE_BUCKET: 'saylani2nd.appspot.com',
+    VITE_MESSUREMNT_ID: 'G-M5S4Q4B51F'
+ }
